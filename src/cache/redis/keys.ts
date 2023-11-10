aea1fcad9ec5f764d8ce72f0ed26c1d944ee1a16
trick <i_line__@naver.com>
2023-11-10 09:07:08 +0900

    modify: cache key 추가

--- src/cache/redis/keys.ts
+++ src/cache/redis/keys.ts
@@ -1,4 +1,5 @@
 export const CACHE_KEYS = {
     DISNITY_USER: (userId: string) => `disnity-user-${userId}`,
     DISCORD_USER: (userId: string) => `discord-user-${userId}`,
+    DISNITY_BOT_ADMINS: (guildId: string) => `disnity-bot-${guildId}-admins`,
 };
